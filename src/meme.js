7318159677ec59eb441c1a74eedb336b181e956f
Ivan Derda <39236703+HobMartin@users.noreply.github.com>
2023-09-15 10:52:42 +0300

    add test commit

--- src/meme.js
+++ src/meme.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 
 async function getMeme() {
   const req = await axios.get('https://meme-api.herokuapp.com/gimme');
+  if(req.data) return req.data
   return req.data;
 }
 
